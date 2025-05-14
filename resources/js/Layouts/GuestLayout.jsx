90dd1ff671761e6446b28066efb7cf6f5f32698c
Igor Boyko <neizvestnyig@yandex.ru>
2025-05-14 19:19:45 +0300

    add image auth

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,6 +1,8 @@
 export default function GuestLayout({ children }) {
     return (
-        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900">
+        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900 bg-no-repeat bg-fixed bg-center bg-cover"
+             style={{ backgroundImage: 'url(assets/bg-train.jpg)', backgroundSize: 'full' }}
+        >
             <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg dark:bg-gray-800">
                 {children}
             </div>
